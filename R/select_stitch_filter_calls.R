40ce66bc2fa3bf0d8e0eff28593e98c251435167
Simone Montalbano <simone.montalbano@pm.me>
2021-07-23 10:18:01 +0000

    added function to select PennCNV calls in specific loci

---
+++ R/select_stitch_filter_calls.R
@@ -0,0 +1,61 @@
+
+select_stich_calls <- function(cnvs, loci, minsnp = 20,
+                               maxgap = 0.5, minoverlap = 0.2) {
+  l <- 1
+
+  for (i in 1:nrow(loci)) {
+    lloc <- loci$locus[i]
+    lchr <- loci$chr[i]
+    lst <- loci$start[i]
+    lsp <- loci$end[i]
+
+    lcnvs <- cnvs[chr == lchr & start <= lsp &
+                  stop >= lst & numsnp >= minsnp, ]
+    lcnvs[, densnp := round(length / numsnp , digits=0)]
+    setorder(lcnvs, c("sample", "type", "start"))
+    lcnvs[, `:=` (stitch = 0, remove = F, gap = NA_numeric_)]
+
+    tmpround <- 1
+    tmpadd <- 1
+
+    while (tmpadd > 0) {
+      inrows <- nrow(lcnvs)
+
+      for (j in 1:nrow(lcnvs)-1) {
+      k <- j+1
+
+      if (lcnvs$sample[j] == lcnvs$sample[k] & lcnvs$type[j] == lcnvs$type[k]) {
+        ggap <- (lcnv[k, start] - lcnvs[j, stop]) / (lcnvs[k, stop] - lcnvs[j, start])
+
+        if (ggap < maxgap) {
+          lcnvs[k, `:=` (sticth = 1, gap = ggap, start = lcnvs[j, start],
+                        length = stop - start + 1, numsnp = numsnp + lcnvs[j, numsnp],
+                        densnp = round(length / numsnp, digits = 0))]
+          lcnvs[j, remove = T]
+        }
+      }
+    }
+
+      lcnvs <- lcnvs[remove == F,]
+      outrows <- nrow(lcnvs)
+      tmpadd <- inrows - outrows
+
+      if (tmpadd > 0) message("round", tmpround)
+      else message("done")
+      tmpround <- tmpround+1
+    }
+
+    lcnvs[, remove := NULL]
+
+    # compute overlap
+    lcnvs[, overlap := (min(stop, lsp) - max(start, lst) + 1) / (lsp-lst+1)]
+    lcnvs[overlap >= minoverlap]
+
+    if (nrow(lcnvs) > 0) {
+      if (l == 1) cnvsOUT <- lcnvs
+      else cnvsOUT <- rbind(cnvsOUT, lcnvs)
+    }
+
+    return(cnvsOUT)
+  }
+}
